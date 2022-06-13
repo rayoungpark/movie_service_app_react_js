1d36f71b73f72b4d200d69fa75a377d2baf7e103
rayoungpark <rayoung1229@gmail.com>
2022-06-13 23:53:53 +0900

    fisit commint

--- src/App.js
+++ src/App.js
@@ -1,23 +1,38 @@
-import logo from './logo.svg';
-import './App.css';
+import Button from "./Button";
+import styles from "./App.module.css";
+import { useEffect, useState } from "react";
 
+function TestNode() {
+  useEffect(() => {
+    console.log("create :)");
+    return () => {
+      // component가 사라질 때 실행됨
+      console.log("destroyed :<");
+    };
+  }, []);
+
+  return <h1>TEST</h1>;
+}
 function App() {
+  const [counter, setCounter] = useState(0);
+  const [keyword, setKeyword] = useState("");
+  const [show, setShow] = useState(false);
+
+  // const hanldeClick = () => setCounter((cur) => cur + 1);
+  const hanldeClick = () => setShow((cur) => !cur);
+  const hanldeInput = (e) => setKeyword(e.target.value);
+
+  useEffect(() => console.log("only one"), []);
+  useEffect(() => {
+    if (keyword && keyword.length > 5) console.log(keyword);
+  }, [keyword]);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      {show && <TestNode />}
+      <input type={"text"} placeholder="Search here..." value={keyword} onChange={hanldeInput} />
+      <p>{counter}</p>
+      <Button onClick={hanldeClick} text={"Button"} />
     </div>
   );
 }
